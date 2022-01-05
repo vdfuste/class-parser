ec88f3549da038569fddc882a3d40654742b5be1
Victor Dual <vdfuste@gmail.com>
2022-01-06 00:16:45 +0100

    Add style casing options

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
-function ClassParser(...args:any[]) {
+enum CaseType {
+	camelCase = "camelCase",
+	"kebab-case" = "kebak-case",
+	lowercase = "lowercase",
+	PascalCase = "PascalCase",
+	snake_case = "snake_case",
+	NONE = "NONE",
+};
+
+let parserCase = CaseType.NONE;
+
+function Parse(...args:any[]) {
 	let newClass:string = "";
 
 	for(let i = 0; i < args.length; i++) {
@@ -8,7 +19,7 @@ function ClassParser(...args:any[]) {
 			|| (typeof arg === "number" && arg > 0)) newClass += arg + " ";
 			else if(Array.isArray(arg) && arg.length) {
 				for(let n = 0; n < arg.length; n++) {
-					const res = ClassParser(arg[n]);
+					const res = Parse(arg[n]);
 					if(res) newClass += res + " ";
 				}
 			}
@@ -17,7 +28,7 @@ function ClassParser(...args:any[]) {
 				for(let n = 0; n < keys.length; n++) {
 					const key = keys[n];
 					if(typeof arg[key] === "boolean" && arg[key]) newClass += key + " ";
-					else newClass += ClassParser(arg[key]);
+					else newClass += Parse(arg[key]);
 				}
 			}
 		}
@@ -26,4 +37,9 @@ function ClassParser(...args:any[]) {
 	return newClass.trim();
 };
 
-export default ClassParser;
+function SetCaseType(caseType:CaseType) {
+	parserCase = caseType;
+}
+
+export default Parse;
+export { SetCaseType, CaseType };
---
+++ test/caseType.test.ts
@@ -0,0 +1,15 @@
+import Parse, * as Parser from "../src";
+
+Test(Parser.CaseType.camelCase, 		"firstExample secondExample thirdExample");
+Test(Parser.CaseType["kebab-case"], 	"first-example second-example third-example");
+Test(Parser.CaseType.lowercase, 		"firstexample secondexample thirdexample");
+Test(Parser.CaseType.PascalCase, 		"FirstExample SecondExample ThirdExample");
+Test(Parser.CaseType.snake_case, 		"first_example second_example third_example");
+
+function Test(type, result) {
+	test(`${type} parsing`, () => {
+		Parser.SetCaseType(type); // Setting the case method
+		expect(Parse({ firstExample: true, "second-example": true, ThirdExample: true }))
+		.toBe(result);
+	});
+}
