e65437939753ce2823350a8e10927a0d754985d3
Víctor Dual <vdfuste@gmail.com>
2022-01-05 15:28:01 -0500

    Create tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,40 @@
 import ClassParser from "../src";
 
-test("Object Parser", () => {
-	expect(ClassParser("Martha"))
-	.toBe("Hello Martha! :D");
+test("Strings Parser", () => {
+	expect(ClassParser("Class", "", null, "parsing"))
+	.toBe("Class parsing");
 });
+
+test("Numbers Parser", () => {
+	expect(ClassParser(0, -1, null, 1))
+	.toBe("1");
+});
+
+test("String Array Parser", () => {
+	expect(ClassParser(["String", "Array", "parsing", "works"]))
+	.toBe("String Array parsing works");
+});
+
+test("Number Array Parser", () => {
+	expect(ClassParser([1, 0, -1, null]))
+	.toBe("1");
+});
+
+test("Object Parser", () => {
+	expect(ClassParser("Object", { fail: false, parsing: true, foo: "works" }))
+	.toBe("Object parsing works");
+});
+
+test("Complex Parser", () => {
+	const arr = [1,1, "abc", true];
+	//const obj = { works: [true, true] };
+	const obj = { works: true, num: 1 };
+
+	expect(ClassParser("Complex", arr, obj))
+	.toBe("Complex parsing works 1");
+});
+
+/*test("Object Parser", () => {
+	expect(ClassParser("Parsing", { fails: false, foo: null, works: true, num: 42 }))
+	.toBe("Parsing works 42");
+});*/
