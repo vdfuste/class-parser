e8db277ff0513c029948235e048bf8e19e2af317
Víctor Dual <vdfuste@gmail.com>
2022-01-05 15:27:36 -0500

    Add logic

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,29 @@
-const ClassParser = (name:string) => `Hello ${name}! :D`;
+function ClassParser(...args:any[]) {
+	let newClass:string = "";
+
+	for(let i = 0; i < args.length; i++) {
+		const arg:any = args[i];
+		if(arg !== null) {
+			if((typeof arg === "string" && arg !== "") 
+			|| (typeof arg === "number" && arg > 0)) newClass += arg + " ";
+			else if(Array.isArray(arg) && arg.length) {
+				for(let n = 0; n < arg.length; n++) {
+					const res = ClassParser(arg[n]);
+					if(res) newClass += res + " ";
+				}
+			}
+			else {
+				const keys = Object.keys(arg);
+				for(let n = 0; n < keys.length; n++) {
+					const key = keys[n];
+					if(typeof arg[key] === "boolean" && arg[key]) newClass += key + " ";
+					else newClass += ClassParser(arg[key]);
+				}
+			}
+		}
+	}
+
+	return newClass.trim();
+};
 
 export default ClassParser;
