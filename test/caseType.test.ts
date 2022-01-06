67c95ef5cc9a7eac016691913419c138b289d641
Victor Dual <vdfuste@gmail.com>
2022-01-06 17:59:17 -0500

    Surround empty message on casing test

--- test/caseType.test.ts
+++ test/caseType.test.ts
@@ -1,12 +1,16 @@
-/*import Parse, * as Parser from "../src";
+//import Parse, * as Parser from "../src";
 
-Test(Parser.CaseType.camelCase, 		"firstExample secondExample thirdExample");
+/*Test(Parser.CaseType.camelCase, 		"firstExample secondExample thirdExample");
 Test(Parser.CaseType["kebab-case"], 	"first-example second-example third-example");
 Test(Parser.CaseType.lowercase, 		"firstexample secondexample thirdexample");
 Test(Parser.CaseType.PascalCase, 		"FirstExample SecondExample ThirdExample");
-Test(Parser.CaseType.snake_case, 		"first_example second_example third_example");
+Test(Parser.CaseType.snake_case, 		"first_example second_example third_example");*/
 
-function Test(type:Parser.CaseType, result:string) {
+describe("Casing tests", () => {
+	test.skip("Skip", () => {});
+});
+
+/*function Test(type:Parser.CaseType, result:string) {
 	test(`${type} parsing`, () => {
 		Parser.SetCaseType(type); // Setting the case method
 		expect(Parse({ firstExample: true, "second-example": true, ThirdExample: true }))
