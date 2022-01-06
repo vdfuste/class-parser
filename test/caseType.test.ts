89772b3c84045c957fdc079d923f5a22e6f2e918
Victor Dual <vdfuste@gmail.com>
2022-01-06 13:51:00 -0500

    Ignore casing tests

--- test/caseType.test.ts
+++ test/caseType.test.ts
@@ -1,4 +1,4 @@
-import Parse, * as Parser from "../src";
+/*import Parse, * as Parser from "../src";
 
 Test(Parser.CaseType.camelCase, 		"firstExample secondExample thirdExample");
 Test(Parser.CaseType["kebab-case"], 	"first-example second-example third-example");
@@ -6,10 +6,10 @@ Test(Parser.CaseType.lowercase, 		"firstexample secondexample thirdexample");
 Test(Parser.CaseType.PascalCase, 		"FirstExample SecondExample ThirdExample");
 Test(Parser.CaseType.snake_case, 		"first_example second_example third_example");
 
-function Test(type, result) {
+function Test(type:Parser.CaseType, result:string) {
 	test(`${type} parsing`, () => {
 		Parser.SetCaseType(type); // Setting the case method
 		expect(Parse({ firstExample: true, "second-example": true, ThirdExample: true }))
 		.toBe(result);
 	});
-}
+}*/
