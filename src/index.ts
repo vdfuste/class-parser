c81560744b54c0125ada07e3ef089956d7e70391
Victor Dual <vdfuste@gmail.com>
2022-01-06 18:00:20 -0500

    Change functionality

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,66 @@
 //import { SetCaseType, CaseType } from "./caseTypes";
 
-function Parse(...args:any[]) {
+function Parse(...args:any[])
+{
 	let newClass:string = "";
 
-	for(let i = 0; i < args.length; i++) {
+	for(let i = 0; i < args.length; i++)
+	{
 		const arg:any = args[i];
-		if(arg !== null) {
-			if((typeof arg === "string" && arg !== "") 
-			|| (typeof arg === "number" && arg > 0)) newClass += arg + " ";
-			else if(Array.isArray(arg) && arg.length) {
-				for(let n = 0; n < arg.length; n++) {
-					const res = Parse(arg[n]);
-					if(res) newClass += res + " ";
+		const type:string = typeof arg;
+
+		if(arg)
+		{
+			if(type === "string" || (type ===  "number" && arg > 0))
+			{
+				newClass += arg + " ";
+			}
+			else if(Array.isArray(arg) && arg.length)
+			{
+				for(let n = 0; n < arg.length; n++)
+				{
+					newClass += Parse(arg[n]) + " ";
 				}
 			}
-			else {
+			else //if(type === "object")
+			{
 				const keys = Object.keys(arg);
-				for(let n = 0; n < keys.length; n++) {
-					const key = keys[n];
-					if(typeof arg[key] === "boolean" && arg[key]) newClass += key + " ";
-					else newClass += Parse(arg[key]);
+
+				for(let k = 0; k < keys.length; k++)
+				{
+					const key = keys[k];
+					const value = arg[key];
+
+					if(typeof value === "boolean" && value)
+					{
+						newClass += key + " ";
+					}
+					else if(Array.isArray(value) && value.length)
+					{
+						let arrayClass = key + " ";
+						
+						for(let n = 0; n < value.length; n++)
+						{
+							if(!value[n])
+							{
+								arrayClass = "";
+								n = value.length;
+							}
+						}
+
+						newClass += arrayClass;
+					}
+					else if(value)
+					{
+						newClass += value + " ";
+					}
 				}
 			}
 		}
 	}
 
 	return newClass.trim();
-};
+}
 
 export default Parse;
 //export { SetCaseType, CaseType };
